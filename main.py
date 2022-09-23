526dc73fec76b1f2de4314401f842b3e7597f6d9
JEN9999 <ekffuclxk@gmail.com>
2022-09-23 09:44:53 +0000

    test2

--- main.py
+++ main.py
@@ -8,23 +8,34 @@ class CoinBot:
     def __init__(self):
         self.client = Client(self.API_KEY, self.API_SECRET, testnet=True)
 
-    # def get_account(self):
-    #     return self.client.get_account()
+    def get_account(self):
+        return self.client.get_account()
 
-    def buy_coin_at_discount(self):
+    def buy_coin_at_discount(self, symbol, quantity):
         #1.코인의 지금가격을 불러온다
-        #3. 그가격에 주문을 넣는다.
-        ticker_info = self.client.get_ticker(symbol="BTCUSDT")
+        ticker_info = self.client.get_ticker(symbol=symbol)
         last_price = ticker_info['lastPrice']
         #2. 코인의 지금가격에서 10% 디스카운트된 가격계산
-        discount_price = float(last_price) * 0.9
+        discount_price = round(int(float(last_price)) * 0.9,
+                               2)  # 1불일경우에 0불에 주문을 넣는 경우가 생김 >>
+        #3. 그가격에 주문을 넣는다.
+        order = self.client.order_limit_buy(symbol=symbol,
+                                            quantity=quantity,
+                                            price=str(discount_price))
 
         print(last_price)
         print(discount_price)
+        print(order)
+        return order
+
+    def buy_coin_at_market_price(self, symbol, quantity):
+        order = self.client.order_market_buy(symbol=symbol, quantity=quantity)
+        return order
 
 
-# bot = CoinBot()
-# print(bot.get_account())
 
 bot = CoinBot()
-bot.buy_coin_at_discount()
+# bot.buy_coin_at_discount('LTCUSDT',1)
+# order_result = bot.buy_coin_at_market_price('LTCUSDT', 1)
+print(bot.client.get_account())
+# print(order_result)
