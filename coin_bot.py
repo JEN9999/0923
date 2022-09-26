e6b2a5bd8df20f6e00a6ce4352443c6d7974648a
JEN9999 <ekffuclxk@gmail.com>
2022-09-26 08:53:29 +0000

    add twitter

--- coin_bot.py
+++ coin_bot.py
@@ -2,7 +2,7 @@ from binance.client import Client
 
 
 class CoinBot:
-  #ToDO : API KEY 코드에서 없애기
+    #ToDO : API KEY 코드에서 없애기
     API_KEY = 'CpTUySnXt57UtWj5wOedmThtXtMZP8FDojxbEgk9d1Iz4mojF3l1VFGuOmeXXB0p'
     API_SECRET = 'cplg01rHhXT8CQCw6yn4w0x9T2ApxN9cPpYlwUSQtJLpUoBDx9g2O68yMyjYVmNL'
 
@@ -21,13 +21,12 @@ class CoinBot:
                                2)  # 1불일경우에 0불에 주문을 넣는 경우가 생김 >>
         #3. 그가격에 주문을 넣는다.
         return self.client.order_limit_buy(symbol=symbol,
-                                            quantity=quantity,
-                                            price=str(discount_price))
+                                           quantity=quantity,
+                                           price=str(discount_price))
 
         print(last_price)
         print(discount_price)
         print(order)
-        
 
     def sell_coin_at_premium(self, symbol, quantity):
         #1.코인의 지금가격을 불러온다
@@ -35,24 +34,21 @@ class CoinBot:
         last_price = ticker_info['lastPrice']
         #2. 코인의 지금가격에서 10% 프리미엄 된 가격계산
         premium_price = round((float(last_price)) * 1.1,
-                               2)  # 1불일경우에 0불에 주문을 넣는 경우가 생김 >>
+                              2)  # 1불일경우에 0불에 주문을 넣는 경우가 생김 >>
         #3. 그가격에 주문을 넣는다.
         return self.client.order_limit_sell(symbol=symbol,
                                             quantity=quantity,
                                             price=str(premium_price))
 
-    
     def cancel_all_open_orders(self):
-      open_orders = self.client.get_open_orders()
-      for order in open_orders:
-        result = self.client.cancel_order(
-          symbol=order['symbol'],
-          orderId=order['orderId'])
-      
-    def sell_coin_at_market_price(self,symbol,quantity):
-        order = self.client.order_market_sell(symbol=symbol,quantity=quantity)
+        open_orders = self.client.get_open_orders()
+        for order in open_orders:
+            result = self.client.cancel_order(symbol=order['symbol'],
+                                              orderId=order['orderId'])
+
+    def sell_coin_at_market_price(self, symbol, quantity):
+        order = self.client.order_market_sell(symbol=symbol, quantity=quantity)
         return order
-        
 
 
 bot = CoinBot()
